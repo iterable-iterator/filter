8c30536f8dfa0400c3bd776276548e8e54acc21b
a flying potato <80830782+a-flying-potato@users.noreply.github.com>
2021-07-14 15:44:57 +0200

    :robot: docs: Use codecov as test link.
    These changes were automatically generated by a transform whose code can be found at:
      - https://github.com/make-github-pseudonymous-again/rejuvenate/blob/042c55f2254a9efdce5074d537141d6f85a98927/src/transforms/docs:use-codecov-as-test-link.js
    Please contact the author of the transform if you believe there was an error.

--- doc/scripts/header.js
+++ doc/scripts/header.js
@@ -17,7 +17,7 @@ domReady(() => {
 	header.insertBefore(projectname, header.firstChild);
 
 	const testlink = document.querySelector('header > a[data-ice="testLink"]');
-	testlink.href = 'https://coveralls.io/github/iterable-iterator/filter';
+	testlink.href = 'https://app.codecov.io/gh/iterable-iterator/filter';
 	testlink.target = '_BLANK';
 
 	const searchBox = document.querySelector('.search-box');
